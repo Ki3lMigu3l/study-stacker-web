1dad8dd10867252e14b2f90b2fb8171cfea6cb61
Ezequiel <ki3lmigu3l@gmail.com>
2023-11-20 13:16:26 -0300

    Jogo Numero Secreto adicionado ao projeto

--- js/swiper.js
+++ js/swiper.js
@@ -9,10 +9,5 @@ const swiper = new Swiper('.swiper', {
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
-    },
-  
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
+    }
   });
